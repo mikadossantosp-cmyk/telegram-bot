1df4fb36fe685eb53148b46ce48e291a4fbed0d3
mikadossantosp-cmyk <mika.dossantos.p@gmail.com>
2026-04-05 08:15:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 import { Telegraf, Markup } from 'telegraf';
 import fs from 'fs';
 
-const BOT_TOKEN = "8406939789:AAHDq3RHOf-nAaUVCL4ZeMduB_KYiBD0i7U";
+const BOT_TOKEN = "8406939789:AAHDq3RHOf-nAaUVCL4ZeMduB_KYiBD0i7M";
 const DATA_FILE = './daten.json';
 const bot = new Telegraf(BOT_TOKEN);
 
